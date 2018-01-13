5d537a3128b00372cb5e7f081eb4adfa59550dd5
Nikola Cekic <nikolacekic@Nikolas-MacBook-Pro.local>
2018-01-12 21:36:48 -0500

    added authorship to pages, removed unnecessary commeted out code that was left behind as a reference, commeted out unused function, commented out automatic win for player one functionality and addedd comment descriptions for it

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+/* Author: Nikola Cekic, 000333667 */
+
 var playerStats = [];
 var player = 1;
 var msgArray = [[],[]];
 var timeToChangeTurns = true;
-//var m = [][];
-var randNum;
+var randNum = 0;
 var counter = 0;
 var flag = false;
 
@@ -12,12 +13,10 @@ var arrayOfBodyPArts = ["tail", "ears", "eyes", "whiskers", "nose"];
 for(var i = 0; i < 2; i++){
     for(var j = 0; j < 6; j++){
 
-        //msgArray[i][j] = (j + 1) + ":";
         msgArray[i][j] = arrayOfBodyPArts[j] + " missing";
         msgArray[i][j].fontcolor("yellow");
     }
 }
-//alert(msgArray[1]);
 
 for(var i = 0; i < 2; i++){
 
@@ -35,14 +34,14 @@ for(var i = 0; i < 2; i++){
     });
 }
 
-function createImage(){
+// function createImage(){
 
-    var imgTag = document.createElement("img");
-    var container = document.getElementsByClassName("box")[0].children[0];
-    container.appendChild(imgTag);
-    imgTag.id = "img";
-    alert("upo");
-}
+//     var imgTag = document.createElement("img");
+//     var container = document.getElementsByClassName("box")[0].children[0];
+//     container.appendChild(imgTag);
+//     imgTag.id = "img";
+//     alert("upo");
+// }
 
 function startRoll(rollButton){
 
@@ -58,24 +57,17 @@ function startRoll(rollButton){
     rollButton.disabled = true;
 
     var imgTag = document.createElement("img");
-    //var imgTag = document.getElementById("img");
-    //var imgTag = document.getElementsByTagName("img")[0];
     imgTag.setAttribute("src", "images/die.gif");
     imgTag.setAttribute("height", "100px");
     imgTag.setAttribute("weight", "100px");
     imgTag.style.marginTop = "0px";
     imgTag.id = "img";
-    // imgTag.setAttribute("margin-top", "500px");
 
     var container = document.getElementsByClassName("box")[0].children[0];
     container.appendChild(imgTag);
 }
 
 function stopRoll(stopButton){
-
-    // .pause {
-    //     animation-play-state: paused;
-    // }
     
     //temp
     rollButton = stopButton.parentElement.parentElement.children[0].children[0];
@@ -84,136 +76,146 @@ function stopRoll(stopButton){
     stopButton.disabled = true;
 
     var imgTag = document.getElementById("img");
-    //var imgTag = document.getElementsByTagName("img")[0];
     imgTag.setAttribute("src", "images/die.png");
     imgTag.setAttribute("height", "60px");
     imgTag.setAttribute("weight", "60px");
-    //imgTag.style.marginTop = "15px";
 
-    //var randNum = Math.floor((Math.random() * 6) + 1);
+    var randNum = Math.floor((Math.random() * 6) + 1);
     
-    //var randNum = 6;
-    randNum = 6;
-    //counter=0;
-    switch(counter){
-        case 1:
-            randNum = 1;
-            break;
-        case 2:
-            randNum = 2;
-            break;
-        case 3:
-            randNum = 3;
-            break;
-        case 4:
-            randNum = 4;
-            break;
-        case 5:
-            randNum = 5;
-    }
+    //the commented out code below can be uncommented and used to automatically make player 1
+    //win the game. This can be useful for testing and debugging.
+    // randNum = 6;
 
-    counter++;
+    // switch(counter){
+    //     case 1:
+    //         randNum = 1;
+    //         break;
+    //     case 2:
+    //         randNum = 2;
+    //         break;
+    //     case 3:
+    //         randNum = 3;
+    //         break;
+    //     case 4:
+    //         randNum = 4;
+    //         break;
+    //     case 5:
+    //         randNum = 5;
+    // }
+
+    // counter++;
     
 
     var bodyPart = "";
     
-    //alert("player " + player);
     switch(randNum){
+
         case 1:
 
-            //msg[0] = "s";
             bodyPart = "tail";
 
-            if(playerStats[player].one == "true")
+            if(playerStats[player].one == "true"){
+
                 createDivsInBoxes(1, "You have already drawn a " + bodyPart + ". Miss your turn.");
+            }
             else if(playerStats[player].bodySelected == "true"){
+
                 playerStats[player].one = "true";
                 timeToChangeTurns = false;
             }
+
             break;
 
         case 2:
 
-            //msg[1] = "s";
             bodyPart = "ears";
 
-            if(playerStats[player].two == "true")
+            if(playerStats[player].two == "true"){
+
                 createDivsInBoxes(1, "You have already drawn " + bodyPart + ". Miss your turn.");
+            }
             else if(playerStats[player].bodySelected == "true"){
+
                 playerStats[player].two = "true";
                 timeToChangeTurns = false;
             }
+
             break;
 
         case 3:
 
-            //msg[2] = "s";
             bodyPart = "eyes";
 
-            if(playerStats[player].three == "true")
+            if(playerStats[player].three == "true"){
+
                 createDivsInBoxes(1, "You have already drawn " + bodyPart + ". Miss your turn.");
+            }
             else if(playerStats[player].bodySelected == "true"){
+
                 playerStats[player].three = "true";
                 timeToChangeTurns = false;
             }
+
             break;
 
         case 4:
 
-            //msg = "s";
             bodyPart = "whiskers";
 
-            if(playerStats[player].four == "true")
+            if(playerStats[player].four == "true"){
+
                 createDivsInBoxes(1, "You have already drawn " + bodyPart + ". Miss your turn.");
+            }
             else if(playerStats[player].bodySelected == "true"){
+
                 playerStats[player].four = "true";
                 timeToChangeTurns = false;
             }
+
             break;
 
         case 5:
 
-            //msg = "s";
             bodyPart = "nose";
 
-            if(playerStats[player].five == "true")
+            if(playerStats[player].five == "true"){
+
                 createDivsInBoxes(1, "You have already drawn a " + bodyPart + ". Miss your turn.");
+            }
             else if(playerStats[player].bodySelected == "true"){
+
                 playerStats[player].five = "true";
                 timeToChangeTurns = false;
             }
+
             break;
 
         case 6:
 
             var bodyPart = "body";
 
-            if(playerStats[player].bodySelected == "true")
+            if(playerStats[player].bodySelected == "true"){
+
                 createDivsInBoxes(1, "You have already drawn a " + bodyPart + ". Miss your turn.");
+            }
             else{
+
                 createDivsInBoxes(1, "Congratulations! You have drawn the body. You can now draw the rest of the mouse.");
                 playerStats[player].bodySelected = "true";
                 timeToChangeTurns = false;
             }
-            break;
-    }
 
-    //playerStats[player].msg[randNum-1] = randNum + ": " + bodyPart + " added!";
-    
+            break;
+    }    
 
     createDivsInBoxes(0, "You have rolled the number: " + randNum + "<br>(" + bodyPart + ")");
-
-
     
     //game can start, body is selected/rolled...everything after goes in here
     if(playerStats[player].bodySelected == "true"){
 
         playerStats[player].msg[randNum-1] = bodyPart + " added!";
-        //alert(player);
         var message = "";
 
-
-        //alert("body selected");
         if(playerStats[player].one == "true" && 
             playerStats[player].two == "true" && 
             playerStats[player].three == "true" && 
@@ -250,45 +252,30 @@ function stopRoll(stopButton){
 
                 playerStats[player].gameWon = true;
                 flag = true;
+
                 createDivsInBoxes(1, "Congratulations, Player " + (player + 1) + "!<br>You Win!");
+                
                 rollButton.disabled = true;
 
                 //create and append play again button
                 var playAgainBtn = document.createElement("button");
                 playAgainBtn.innerHTML = "Play Again"
-                //playAgainBtn
-                //playAgainBtn.setAttribute("class", "btn btn-success");
                 playAgainBtn.className = "btn btn-success";
                 playAgainBtn.style.marginTop = "25px";
-                //
-                //playAgainBtn.onclick = window.location.reload;
-                //playAgainBtn.setAttribute("click", location.reload);
-                //playAgainBtn.setAttribute("click", reLoad());
                 playAgainBtn.onclick = reLoad;
 
-                //alert(document.getElementById("boxContainer").children[1].innerHTML);
-                //document.getElementById("boxContainer").children[1].appendChild = playAgainBtn;
                 var x = document.getElementById("boxContainer").children[1];
                 console.log()
                 x.appendChild(playAgainBtn);
 
-            } 
-        //else if(timeToChangeTurns == "false"){
+        } 
         else{
-            //var message = "";
 
-            for(var i = 0; i < 6; i++)
-                //m=
-
-            for(var i = 0; i < playerStats[player].msg.length; i++){
+            for(var i = 0; i < playerStats[player].msg.length; i++)
                 message = message + playerStats[player].msg[i] + "<br>";
-                //message.fontcolor("yellow");
-            }
 
             createDivsInBoxes(1, message);
         }
-        //alert("message");
-        //createDivsInBoxes(1, message);
     }
     else{
 
@@ -298,20 +285,16 @@ function stopRoll(stopButton){
 
 function createDivsInBoxes(indexOfBox, msg){
 
-    //var divTag = document.createElement("div");
     var divTag = document.getElementsByClassName("box")[indexOfBox].children[0];
     
-    //divTag.style.color = "white";
     if(indexOfBox == 0){
 
         divTag.innerHTML = divTag.innerHTML + "<br><br>" + msg;
         divTag.style.marginTop = "25px";
         divTag.style.color = "white";
-        //divTag.innerHTML = msg;
     }
     else{
 
-        //alert(msg);
         if(flag){
 
             divTag.innerHTML = msg;
@@ -333,8 +316,6 @@ function createDivsInBoxes(indexOfBox, msg){
                     
                     p.style.color = "pink";
                     divTag.appendChild(p);
-                    //msg[i].color("red");
-                    //alert(msg[i]);
                 }
                 else if(!msg[i].includes("Congratulations")){
 
@@ -344,9 +325,7 @@ function createDivsInBoxes(indexOfBox, msg){
 
                 
             }
-    }
-            //alert(msg[i]);
-        //divTag.innerHTML = msg;
+        }
     }
 
     var container = document.getElementsByClassName("box")[indexOfBox];
@@ -358,39 +337,6 @@ function clearDivsInBoxes(){
     for(var i = 0; i < 2; i++)
         document.getElementsByClassName("box")[i].children[0].innerHTML = "";
 }
-
-    // var divTag1 = document.createElement("div");
-    // divTag1.style.marginTop = "25px";
-    // divTag1.style.color = "white";
-    // divTag1.innerHTML = "You have rolled the number: " + randNum;
-
-    // var divTag2 = document.createElement("div");
-    // divTag2.style.marginTop = "25px";
-    // divTag2.style.color = "white";
-    // divTag2.innerHTML = msg;
-
-
-    // var container1 = document.getElementById("box1");
-    // container1.appendChild(divTag1);
-
-    // var container2 = document.getElementById("box2");
-    // container2.appendChild(divTag2);
-
-    // alert(container.innerHTML);
-    // container.setAttribute("padding-top", "100px !important");
-    
-    
-    // alert(img);
-    // img.setAttribute("animation-play-state", "paused !important");
-
-    // var imgTag = document.createElement("img");
-    // imgTag.setAttribute("src", "die.jpg");
-    // imgTag.setAttribute("height", "100px");
-    // imgTag.setAttribute("weight", "100px");
-    // imgTag.setAttribute("margin-top", "500px");
-
-    // var container = document.getElementById("box");
-    // container.appendChild(imgTag);
 
 function changePlayer(){
 
